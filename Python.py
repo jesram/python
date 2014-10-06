bf8be6893540e6b6579766ce6e2d5a00ac72040f
Jesus Ramon <jr281990@gmail.com>
2014-10-06 17:57:44 -0500

    1843 added

--- Python.py
+++ Python.py
@@ -4,7 +4,7 @@
     n2 += US(n / 10, n2)
     return n2 + n % 10
 '''
-def US(n):
+'''def US(n):
     if n < 10:
         return n
     num = 0
@@ -21,4 +21,24 @@ while T > 0:
         a = US(N)
         N = a
     print a
-    T -= 1
+    T -= 1'''
+
+a, b, c = 0, 0, 0
+while True:
+    c += 1
+    y = raw_input()
+    y2 = y.split(' ')
+    a, b = int(y2[0]), int(y2[1])
+    if a == 0 and b == 0:
+        break
+    x, c2, s, s2 = 0, 0, 1, 1
+    i = 0
+    while x + 1 < b:
+        x += i
+        while s < x + 1:
+            s2 += 1
+            s = s2 * s2
+        if x + 1 > a and x + 1 < b and x + 1 == s:
+            c2 += 1
+        i += 1
+    print "Case {0}: {1}".format(c, c2)
